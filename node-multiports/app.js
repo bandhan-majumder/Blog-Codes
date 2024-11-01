564c84fc958711551976a7d965ac90255dba549e
bandhan-majumder <bandhanmajumder16@gmail.com>
2024-11-01 13:03:18 +0530

    changed to express application

--- node-multiports/app.js
+++ node-multiports/app.js
@@ -1,23 +1,30 @@
-const http = require('http'); 
- 
-// Create a server for port 3000 
-const server1 = http.createServer((req, res) => { 
-    res.writeHead(200, { 'Content-Type': 'text/plain' }); 
-    res.end('Hello from server on port 3000\n'); 
-}); 
- 
-// Create a server for port 5000 
-const server2 = http.createServer((req, res) => { 
-    res.writeHead(200, { 'Content-Type': 'text/plain' }); 
-    res.end('Hello from server on port 5000\n'); 
-}); 
- 
-// Start listening on port 3000 
-server1.listen(3000, () => { 
-    console.log('Server listening on port 3000'); 
-}); 
- 
-// Start listening on port 5000 
-server2.listen(5000, () => { 
-    console.log('Server listening on port 5000'); 
+const express = require('express');
+
+const app1 = express();
+const app2 = express();
+
+const PORT1 = "3000"
+const PORT2 = "3002"
+
+
+// route handlers
+app1.get("/",(req,res)=>{
+    res.json({
+        "msg": `greetings from port ${PORT1}`
+    })
+})
+
+app2.get("/",(req,res)=>{
+    res.json({
+        "msg": `greetings from port ${PORT2}`
+    })
+})
+
+// listen
+app1.listen(`${PORT1}`, () => {
+  console.log(`Started server on ${PORT1}`);
+});
+
+app2.listen(`${PORT2}`, () => {
+  console.log(`Started server on ${PORT2}`);   
 });
